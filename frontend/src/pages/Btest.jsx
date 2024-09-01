40066d06b15236fd99bd1ab96d547ed329a96c58
ajaymude <mudeajay@gmail.com>
2024-09-01 17:48:09 +0530

    test branch is deleted

--- frontend/src/pages/Btest.jsx
+++ frontend/src/pages/Btest.jsx
@@ -2,7 +2,7 @@
 const Btest = () => {
   
   return (
-    <div>Btest this change for the git branch merge tesing functionality git merger test3 with the test  </div>
+    <div>Btest this change for the git branch merge tesing functionality git merger test3 with the test branch is deleted  </div>
   )
 }
 
